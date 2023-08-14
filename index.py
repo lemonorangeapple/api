97f5c388a050aafeed3f447b30a15ee78dccabe2
Jiang Chengjun <107529938+lemonorangeapple@users.noreply.github.com>
2023-08-14 15:28:29 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+import requests
+from flask import *
+from gevent import monkey
+monkey.patch_all()
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    return "200 OK"
